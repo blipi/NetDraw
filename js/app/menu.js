1f1ffbeca619f0112d5fac3ef522b7ca9004afcd
Blipi <gpascualg93@gmail.com>
2015-03-16 16:52:08 +0100

    Redesign the whole menu and interface

--- js/app/menu.js
+++ js/app/menu.js
@@ -2,10 +2,10 @@ define(['jquery', 'protobuf.2', 'app/layer', 'app/relationship', 'app/controller
 
     var canvas = controller.getCanvas();
     var instances = {byGroup: {}, byBox: {}};
-    var menuY = 50;
+    var currentCategory = null;
 
-    var MenuInstance = function (_box, _layers, _triggered) {
-        return {box: _box, layers: _layers, triggered: _triggered};
+    var MenuInstance = function (_category, _triggered) {
+        return {category: _category, triggered: _triggered};
     };
 
     var Menu = {
@@ -70,7 +70,8 @@ define(['jquery', 'protobuf.2', 'app/layer', 'app/relationship', 'app/controller
                 controller.getWrapper().css('z-index', '-1');
             };
 
-            var menuObj = $('#menu');
+            var menuObj = $('.menu');
+            var menuList = $('.menu .categories ul');
 
             // Import button
             canvas.drawBoxInto(menuObj, {
@@ -81,80 +82,80 @@ define(['jquery', 'protobuf.2', 'app/layer', 'app/relationship', 'app/controller
             });
 
             var menu_onclick = function () {
-                var group = instances.byBox[$(this).attr('id')];
-                var instancesByGroup = instances.byGroup;
-                var instance = instancesByGroup[group];
+                var me = $(this);
 
-                if (!instance.triggered) {
-                    /* Expand */
-                    for (var g in instancesByGroup) {
+                if (!currentCategory || currentCategory.data('container') != $(this).data('container')) {
+                    var showCurrent = function () {
+                        $('#' + me.data('container'))
+                            .show()
+                            .css('animation-holder', '0')
+                            .animate({
+                                'animation-holder': 90
+                            },
+                            {
+                            duration: 150,
+                            step: function (now, fx) {
+                                    $(this).css('transform', 'rotateX(' + (-90 + now) + 'deg)');
+                                }
+                            });
+                    };
 
-                        instancesByGroup[g].box.animate({
-                            top: menuY - 3
-                        }, 'medium', 'swing');
-
-                        if (instancesByGroup[g].box.attr('id') == $(this).attr('id')) {
-                            instancesByGroup[g].box.css('z-index', 2);
-                        } else {
-                            instancesByGroup[g].box.css('z-index', 1);
-                        }
+                    if (currentCategory) {
+                        $('#' + currentCategory.data('container'))
+                            .show()
+                            .css('animation-holder', '0')
+                            .animate({
+                                'animation-holder': 90
+                            },
+                            {
+                            duration: 150,
+                            step: function (now, fx) {
+                                    $(this).css('transform', 'rotateX(' + (now) + 'deg)');
+                                },
+                            done: showCurrent
+                        });
+                    } else {
+                        showCurrent();
                     }
 
-                    for (var i = 0, len = instance.layers.length; i < len; ++i) {
-                        instance.layers[i].visible = true;
-                    }
-                } else {
-                    var ey = menuY;
-
-                    for (var i = 0, len = instance.layers.length; i < len; ++i) {
-                        instance.layers[i].visible = false;
-                    }
-
-                    /* Expand */
-                    for (var g in instancesByGroup) {
-
-                        instancesByGroup[g].box.animate({
-                            top: ey - 3
-                        }, 'medium', 'swing');
-
-                        ey += 27;
-                    }
+                    currentCategory = me;
                 }
 
-                instance.triggered = !instance.triggered;
             };
 
-            var ey = menuY;
             var id = 0;
             for (var group in groups) {
 
-                /* Box */
-                var box = canvas.drawBoxInto(menu, {
-                    x: -3, y: -3 + ey,
-                    click: menu_onclick,
-                    text: group,
-                    id: 'box-' + id,
-                    className: 'menu-box'
-                });
+                var category = $('<li>' + group + '</li>')
+                    .click(menu_onclick)
+                    .data('container', 'category-' + id)
+                    .appendTo(menuList);
+
+                var container = $('<div id="category-' + id + '" class="category-container"></div>').
+                    appendTo(menuObj);
 
                 console.log('================');
                 console.log(group);
 
-                var layers = [];
-                var i = 0;
-                for (var k = 0, len = groups[group].length; k < len; ++k) {
+                var len = len = groups[group].length;
+                var x = parseInt(menuObj.css('width')) / 2 - (len * 100 + (len - 1) * 20) / 2;
+
+                for (var k = 0; k < len; ++k) {
                     var layerType = UpgradeV1LayerType(parseInt(groups[group][k]));
 
-                    layers.push(layer.create(25, menuY + 25 + 60 * i, layerType, false, menu, false));
-                    i += 1;
+                    layer.create(x, 18, layerType, true, container, false);
+                    x += 120;
                 }
 
-                instances.byGroup[group] = MenuInstance(box, layers, false);
-                instances.byBox['box-' + id] = group;
+                instances.byGroup[group] = MenuInstance(category, false);
+                instances.byBox['category-' + id] = category;
 
-                ey += 27;
                 ++id;
             }
+
+            $(function () {
+
+            });
         }
     };
 
